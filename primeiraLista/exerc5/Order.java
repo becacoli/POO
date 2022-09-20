b43d9bffe6d612ed27b52bce14a0e064150da7ef
beqa <rebeca.correia100@gmail.com>
2022-09-20 11:41:13 -0300

    iniciando o exerc5

---
+++ primeiraLista/exerc5/Order.java
@@ -0,0 +1,10 @@
+package exerc5;
+
+import java.util.ArrayList;
+
+public class Order {
+  private java.util.Date moment;
+  private OrderStatus status;
+  private Client client;
+  private ArrayList<OrderItem> items;
+}
