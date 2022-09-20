7a45e14258caf7f7e79a6101529b02e4f15a90ba
beqa <rebeca.correia100@gmail.com>
2022-09-20 08:42:27 -0300

    exerc3 resolvido

---
+++ primeiraLista/exerc3/Employee.java
@@ -0,0 +1,46 @@
+package exerc3;
+
+public class Employee {
+  private String name;
+  private double grossSalary;
+  private double tax;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getGrossSalary() {
+    return grossSalary;
+  }
+
+  public void setGrossSalary(double grossSalary) {
+    this.grossSalary = grossSalary;
+  }
+
+  public double getTax() {
+    return tax;
+  }
+
+  public void setTax(double tax) {
+    this.tax = tax;
+  }
+
+  public double netSalary() {
+    return grossSalary - tax;
+  }
+
+  public void increaseSalary(double percentage) {
+    grossSalary = grossSalary + (grossSalary * (percentage / 100));
+  }
+
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return String.format("%s, $ %.2f", name, netSalary());
+  }
+
+}
