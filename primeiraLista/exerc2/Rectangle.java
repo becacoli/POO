6fa26b309a63858ec86eef33ddcf6c798b762fd4
beqa <rebeca.correia100@gmail.com>
2022-09-20 07:31:07 -0300

    concluindo exerc2

--- primeiraLista/exerc2/Rectangle.java
+++ primeiraLista/exerc2/Rectangle.java
@@ -1,5 +1,18 @@
 package exerc2;
 
 public class Rectangle {
-  
+  double width, height;
+
+  double area() {
+    return width * height;
+  }
+
+  double perimeter() {
+    return (width * height) * 2;
+  }
+
+  double diagonal() {
+    return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+  }
 }
