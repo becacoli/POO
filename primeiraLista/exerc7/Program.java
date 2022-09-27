cd86d8f275a9ddde2d7f6124678c09c22fcd9da6
beqa <rebeca.correia100@gmail.com>
2022-09-26 21:10:42 -0300

    arrumando o exerc7

--- primeiraLista/exerc7/Program.java
+++ primeiraLista/exerc7/Program.java
@@ -4,9 +4,11 @@ public class Program {
   public static void main(String[] args) { 
     YoutubeVideo youtubeVideo = new YoutubeVideo(); 
     FaceVideo faceVideo = new FaceVideo(); 
-    VideoPlayer videoPlayer = new VideoPlayer(youtubeVideo);
-    videoPlayer.play();
-    videoPlayer.setVideo(faceVideo);
-    videoPlayer.play();
+
+    VideoPlayer player = new VideoPlayer(faceVideo);
+    player.play();
+    
+    VideoPlayer player2 = new VideoPlayer(youtubeVideo);
+    player2.play();
 }
 }
